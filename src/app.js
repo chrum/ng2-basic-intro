e02f535148c4e8e8a370d62de56e8126742dcc9f
chrystian <chrystianr@gmail.com>
2016-06-18 13:05:11 +0200

    Dynamic slides loading

--- src/app.js
+++ src/app.js
@@ -2,30 +2,55 @@ window.$ = window.jQuery = require('./libs/jquery');
 
 
 $(document).ready(function () {
-    $('#simple').jmpress();
-    /*loadTemplates(() => {
+    let root = $('#simple');
+    templateLoader.init(root, () => {
+        root.jmpress();
 
-    })*/
+    });
 });
 
+let templateLoader = {
+    _root: null,
+    _templates: [],
+    _afterLoaded: function () {},
+    init: function (root, callback) {
+        this._root = root;
+        this._afterLoaded = callback;
 
-function loadTemplates(onTemplatesLoaded) {
-    var normalizedPath = require ('path').join (__dirname, 'src/slides');
-    let templates = {};
-    let read = 0;
-    let fs = require ('fs');
-    fs.readdirSync (normalizedPath).forEach ((el, index, arr) => {
-        let name = el.replace ('.html', '');
-        fs.readFile (normalizedPath + '/' + el, 'utf8', (err, html) => {
-            read++;
-            templates[name] = html;
+        this._loadTemplates();
+    },
 
-            if (read === arr.length) {
-                onTemplatesLoaded();
-            }
+    _render() {
+        this._templates.sort((a, b) => {
+            a.id > b.id
+        }).map((item) => {
+            this._root.append(item.html);
         });
-    });
-}
+
+        this._afterLoaded();
+    },
+
+    _loadTemplates: function () {
+        var normalizedPath = require ('path').join (__dirname, 'src/slides');
+        let read = 0;
+        let fs = require ('fs');
+        fs.readdirSync (normalizedPath).forEach ((el, index, arr) => {
+            let name = el.replace ('.html', '');
+            fs.readFile (normalizedPath + '/' + el, 'utf8', (err, html) => {
+                read++;
+                this._templates.push({
+                    id: parseInt(name),
+                    html: html
+                });
+
+                if (read === arr.length) {
+                    this._render();
+                }
+            });
+        });
+    }
+};
+
 
 
 
